1e6bc35a4fe11c28dbb3c7fc9ded637ca99c337c
Daniel Morgan <daniel.morgan@unitasglobal.com>
2018-04-11 16:56:14 -0500

    more changes

--- aws/setup.js
+++ aws/setup.js
@@ -1,22 +1,62 @@
 
-
+const os = require('os');
 const fs = require('fs');
 
 let TRANSLATION = {}
-
-
 
 
 let fileList = [];
 
 const fileData = {};
 
+const getOSData = () => {
+	let data = {
+		cores:os.cpus().length,
+		freemem:os.freemem(),
+		totalmem:os.totalmem(),
+		hostname:os.hostname(),
+		arch:os.arch(),
+		platform:os.platform(),
+		release:os.release(),
+		net_inf:os.networkInterfaces(),
+
+	};
+	return data;
+
+};
+
+const makeNetTable = (data) => {
+	var rows = [];
+	for(var inf in data){
+		let subcols = [];
+		data[inf].forEach((subdata) => {
+			let col = "<table>";
+				col += `<tr><th>${subdata.family}</th></tr>`;
+				col += `<tr><td>ip address:${subdata.address}</td></tr>`;
+				col += `<tr><td>network mask:${subdata.netmask}</td></tr>`;
+				col += `<tr><td>mac address:${subdata.mac}</td></tr>`;
+				col += `<tr><td>cidr:${subdata.cidr}</td></tr>`;
+				col += "</table>";
+				subcols.push(col);
+		});
+		let row = `<tr><td><b>${inf}</b></td><td>${subcols.join("</td><td>")}</td>`;
+		rows.push(row);
+	}
+	return `<table>${rows.join("")}</table>`;
+}
+
+
 const makeTable = (data) => {
 	let rows = [];
 	for(var key in data){
 		var value = data[key];
 		if(!!value && !!TRANSLATION[key]){
+			if(key == "net_inf"){
+				value = makeNetTable(value);
+			}
 			let row = `<tr><th>${TRANSLATION[key]}</th><td>${value}</td></tr>`;
+
+
 			rows.push(row);
 		}
 	}
@@ -41,7 +81,7 @@ const readFiles = (callback) => {
 }
 
 const processFiles = () => {
-	console.log(fileData);
+
 	TRANSLATION = JSON.parse(fileData.translation);
 	let aws_settings = JSON.parse(fileData.aws_settings);
 	delete fileData.aws_settings;
@@ -56,12 +96,12 @@ const processFiles = () => {
 		}
 
 	}
-	console.log(JSON.stringify(aws_settings,null,4));
+
 
 	let html = fileData["template.html"];
 	html = html.replace("<!--CLOUD SETTINGS-->",makeTable(aws_settings));
 	html = html.replace("<!--APP SETTINGS-->",makeTable(aws_settings));
-	html = html.replace("<!--OS SETTINGS-->",makeTable(aws_settings));
+	html = html.replace("<!--OS SETTINGS-->",makeTable(getOSData()));
 	fs.writeFile("index.html",html,(err) => {
 		console.log("done");
 	});
