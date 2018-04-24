6474441ee8cba335a4ee3932e3be3a6ba7e42e9e
Daniel Morgan <daniel.morgan@unitasglobal.com>
2018-04-24 15:15:57 -0500

    Added support for custom data

--- azure/setup.sh
+++ azure/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+cd /ug/ugprov;
+
+git pull;
+
+
 cat  /var/lib/waagent/ovf-env.xml > /ug/ugprov/azure/ovf.xml;
 
 curl -H Metadata:true "http://169.254.169.254/metadata/instance?api-version=2017-08-01" > azure_settings;
